d4d67eb08b20f9ae9856b62fda81ddf0f6b51094
manley1595 <manley1595@gmail.com>
2026-02-21 15:27:52 -0600

    Delete badge.jsx

--- badge.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { cn } from './utils'
-export function Badge({ className, ...props }) {
-  return (
-    <span className={cn('inline-flex items-center rounded-full px-3 py-1 text-xs font-medium bg-emerald-700/10 text-emerald-900 border border-emerald-700/20', className)} {...props} />
-  )
-}
